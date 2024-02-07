8387d7c46cef410f2804a76b284b201cb67bad2b
coolsam726 <coolsam726@users.noreply.github.com>
2024-02-07 10:09:08 +0000

    Fix styling

--- src/Concerns/Filament/SystemSetupResource.php
+++ src/Concerns/Filament/SystemSetupResource.php
@@ -4,18 +4,14 @@ namespace Vanadi\Framework\Concerns\Filament;
 
 use Filament\Resources\Resource;
 use Vanadi\Framework\FrameworkPlugin;
-use function Vanadi\Framework\framework;
 
 /**
  * Trait SystemSetupResource
- * @package Vanadi\Framework\Concerns\Filament
+ *
  * @mixin Resource
  */
 trait SystemSetupResource
 {
-    /**
-     * @return string|null
-     */
     public static function getNavigationGroup(): ?string
     {
         return FrameworkPlugin::getNavigationGroupLabel();
