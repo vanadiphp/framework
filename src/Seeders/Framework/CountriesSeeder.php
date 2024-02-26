49589b3ca235774b98f2e5d6466331353a64af85
Sam Maosa <maosa.sam@gmail.com>
2024-02-26 15:01:38 +0300

    Use Relative Paths for flag svg

--- src/Seeders/Framework/CountriesSeeder.php
+++ src/Seeders/Framework/CountriesSeeder.php
@@ -5,6 +5,7 @@ namespace Vanadi\Framework\Seeders\Framework;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 use PragmaRX\Countries\Package\Countries;
 use Vanadi\Framework\Helpers\Access;
 use Vanadi\Framework\Models\Country;
@@ -26,13 +27,16 @@ class CountriesSeeder extends Seeder
                     $currency = null;
                 }
                 $this->command->comment("Seeding $code");
+                $flagPath = $country->get('flag.svg_path');
+               $flag = Str::substr($flagPath,  Str::position($country->get('flag.svg_path'), '/vendor/'));
                 Country::query()->firstOrCreate(['code' => $code, 'team_id' => default_team()->id], [
                     'cca2' => $country->get('cca2'),
                     'cca3' => $country->get('cca3'),
                     'name' => $country->get('name.common'),
                     'capital' => $country->get('capital.0'),
                     'flag_emoji' => $country->get('flag.emoji'),
-                    'flag_svg_path' => $country->get('flag.svg_path'),
+                    // extract only the path from vendor/ onwards
+                    'flag_svg_path' => $flag,
                     'currency_code' => $currency,
                 ]);
             });
